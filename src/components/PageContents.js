29bd7164bac0e7cb76b1359ceeb9f014addc7082
Justinas <156369263+justinnas@users.noreply.github.com>
2024-02-09 02:48:35 +0200

    Added scaling down of the main text

--- src/components/PageContents.js
+++ src/components/PageContents.js
@@ -18,19 +18,46 @@ export default function PageContents() {
 			>
 				<Grid item xl={6} md={12} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 					<Box style={{ overflow: 'visible', alignItems: 'center', justifyContent: 'center', width: '500px' }}>
-						<p style={{ fontWeight: '100', fontSize: '2.5em', marginBottom: '-10px' }}>Hello, I'm</p>
-						<p style={{ fontWeight: '900', fontSize: '4em' }}>JUSTINAS;</p>
-						<p style={{ fontWeight: '700', fontSize: '2.5em', marginTop: '1em', whiteSpace: 'nowrap' }}>
+						<Typography
+							sx={{
+								fontSize: { xl: '2.5rem', md: '1rem' },
+								fontWeight: '100',
+								marginBottom: '-10px',
+								fontFamily: 'Poppins',
+							}}
+						>
+							Hello, I'm
+						</Typography>
+						<Typography sx={{ fontSize: { xl: '4rem', md: '2rem' }, fontWeight: '900', fontFamily: 'Poppins' }}>
+							JUSTINAS;
+						</Typography>
+						<Typography
+							sx={{
+								marginTop: '1em',
+								whiteSpace: 'nowrap',
+								fontFamily: 'Poppins',
+								fontSize: { xl: '2.5rem', md: '1.5rem' },
+								fontWeight: '900',
+							}}
+						>
 							<Grid container>
 								<Grid item xl={3.7} md={12}>
 									Need a
 								</Grid>
-								<Grid item xl={8.3} md={12}>
+								<Grid item xl={8.3} md={12} sx={{ fontSize: { xl: '2.5rem', md: '1.5rem' }, fontWeight: '900' }}>
 									<TitleAnimation />?
 								</Grid>
 							</Grid>
-						</p>
-						<ContactSites />
+						</Typography>
+						<Box
+							sx={{
+								scale: { xl: '1', md: '0.6', xs: '0.2' },
+								marginLeft: { xl: '0', md: '-25%' },
+								marginTop: { xl: '0', md: '-3%' },
+							}}
+						>
+							<ContactSites />
+						</Box>
 					</Box>
 				</Grid>
 				<Grid item xl={6} md={12} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
